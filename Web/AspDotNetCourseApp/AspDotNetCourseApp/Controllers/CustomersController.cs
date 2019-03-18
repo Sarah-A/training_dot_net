0620bcc7d6708a201fde919ebeb35c9c42ef87dd
SarahA <ashri.sarah@gmail.com>
2019-03-18 13:48:21 +1000

    New Connected to SQL Server Database (code-first) and added some data to the MembershipTypes and Customers tables.

--- Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/CustomersController.cs
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/CustomersController.cs
@@ -4,18 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AspDotNetCourseApp.Models;
+using System.Data.Entity;
+
 
 namespace AspDotNetCourseApp.Controllers
 {
     public class CustomersController : Controller
     {
+        // in order to be able to query the database in the CustomersController, we need
+        // to connect it to the applicaiton database using a private field:
+        // We also need to initialize it in the constructor and to override Controller.Dispose
+        // function to dispose it:
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+
+
 
         private List<Customer> GetCustomers()
         {
-            return new List<Customer> {
-            new Customer() { Name = "Customer 1", Id = 1},
-            new Customer() { Name = "Customer 2", Id = 2}
-            };
+            return _context.Customers.Include(c => c.MembershipType).ToList();           
         }
 
         // GET: Customers
