485b7a19a4fa6c2fa9769fd6b8c050e468ba8127
SarahA <ashri.sarah@gmail.com>
2019-03-18 10:12:03 +1000

    New: Identity Framework and Entity Framework support for adding database

---
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspDotNetCourseApp.Startup))]
+namespace AspDotNetCourseApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
