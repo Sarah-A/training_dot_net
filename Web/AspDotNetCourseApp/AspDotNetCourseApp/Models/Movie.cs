37ef2234eb722d1a172ba26f9933940f047edb7c
SarahA <ashri.sarah@gmail.com>
2019-03-19 12:19:51 +1000

    New: Fields in Movie Model.

--- Web/AspDotNetCourseApp/AspDotNetCourseApp/Models/Movie.cs
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Models/Movie.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspDotNetCourseApp.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        public DateTime ReleaseDate { get; set; }
+        public DateTime AddedToDb { get; set; }
         public Genre Genre { get; set; }
         public byte GenreId { get; set; }
+        public byte NumberInStock { get; set; }
     }
 }
