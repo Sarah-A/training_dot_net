02c4e8ecf3a35befd82757d06a0b10f852f969cc
SarahA <ashri.sarah@gmail.com>
2019-03-27 13:39:53 +1000

    AspDotNetCourseApp - Refactoring only - cleaned up MoviesController from redundant methods that aren't in use.

--- Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/MoviesController.cs
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/MoviesController.cs
@@ -8,8 +8,7 @@ using AspDotNetCourseApp.ViewModels;
 using System.Data.Entity;
 
 namespace AspDotNetCourseApp.Controllers
-{
-    [Authorize(Roles ="CanManageMovies")]
+{    
     public class MoviesController : Controller
     {
         private ApplicationDbContext _context;
@@ -33,69 +32,12 @@ namespace AspDotNetCourseApp.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            return View();          
-        }
-
-        // Example for using View to send the model (Movie) to the view:
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);            
-        }
-
-        // Example of using ViewData to send data (partial or calculated) to the view:
-        public ActionResult RandomAsViewData()
-        {
-            var movie = new Movie() { Name = "Shrek -1!" };
-            ViewData["Movie"] = movie;
-            return View();
-        }
-
-        // Example of using ViewBag  - a newer way than ViewData to send data (partial or calculated) to the view:
-        public ActionResult RandomAsViewBag()
-        {
-            var movie = new Movie() { Name = "Shrek 2!" };
-            ViewBag.MovieName = movie.Name;
-            return View();
-        }
-
-        // Example of using ViewModel  - the most recommended and type-safe way:
-        public ActionResult RandomAsViewModel()
-        {
-            var moviesRandom = new MoviesRandomViewModel()
-            {
-                Movie = new Movie() { Name = "Shrek - the best!! " },
-                Customers = new List<Customer>{
-                    new Customer() { Name = "Customer 1" },
-                    new Customer() { Name = "Customer 2" }
-                }
-            };
-
-            return View(moviesRandom);
-        }
-               
-
-        [Route("movies/releases/{year:regex(\\d{4}):range(1900,2019)}/{month:regex(\\d{1-2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {            
-            return Content($"year: {year} , month: {month}");
-        }
-
-        [Route("movies/details/{id}")]
-        public ActionResult Details(int id)
-        {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
-
-            if (movie == null)
-            {
-                return Content($"Movie ID: ${id} not found!");
-            }
-            else
-            {
-                return View(movie);
-            }
-        }
-
+			return View();
+		
+         }
+            
+       
+        
         public ActionResult MovieForm()
         {
             var viewModel = new MovieFormViewModel()
