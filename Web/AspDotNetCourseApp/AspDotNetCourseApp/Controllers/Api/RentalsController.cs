aeb70bc5b4d7754597d5f7cee2e6e0e3ffe4a598
SarahA <ashri.sarah@gmail.com>
2019-03-29 11:21:21 +1000

    AspDotNetCourseApp - Adding Feature E2E - 3. Implemented the happy path of the PostNewRental - assuming the movie is available for renting.
    Note: if the movieId or customerId is invalid - the app will throw an exception as it should since this should never happen, assuming the user is using the application front end to choose the customer and rentals and not sending random data to the server (hacker).

--- Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/Api/RentalsController.cs
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Controllers/Api/RentalsController.cs
@@ -1,4 +1,5 @@
 ﻿using AspDotNetCourseApp.Dtos;
+using AspDotNetCourseApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,19 @@ namespace AspDotNetCourseApp.Controllers.Api
 {
     public class RentalsController : ApiController
     {
+        ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);    
+        }
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
@@ -26,7 +40,35 @@ namespace AspDotNetCourseApp.Controllers.Api
         [HttpPost]
         public IHttpActionResult PostNewRentals(RentalDto rentalDto )
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            // Note: we use Single and not SingleOrDefault here since the user will pick up the 
+            // customer from a list so we don't expect this to fail. Exception will be thrown if 
+            // it does fail which is the correct way to handle unexpected fails.
+            var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            
+            var movies = _context.Movies
+                                 .Where(m => rentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {               
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Today
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            // since we didn't create a single new resource and we just want to notify the user
+            // that the action was successful, we don't return the Created but instead return Ok();
+            return Ok();
+            
         }
 
         // PUT api/<controller>/5
