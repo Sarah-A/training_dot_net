eb3c5f49c0dc29df0c6a3a2c5c6ca3e6743bfab3
SarahA <ashri.sarah@gmail.com>
2019-04-03 08:30:54 +1000

    AspDotNetCoreFromScratch - New Project for Asp.Net Core Course
    This is the initial project after starting a new project and choosing ASP.Net Core 2.2 with an empty project.

---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspDotNetCoreFromScratch
+{
+    public class Program
+    {
+        // At its core, this is just a console application that listens to web requests.
+        public static void Main(string[] args)
+        {
+            // Building the web host and start listenning on it.
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
