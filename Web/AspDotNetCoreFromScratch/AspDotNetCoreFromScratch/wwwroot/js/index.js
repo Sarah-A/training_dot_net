897a6b0af5f4f4fe980056dc6061c7be33e5760d
SarahA <ashri.sarah@gmail.com>
2019-04-03 11:37:21 +1000

    AspDotNetCoreFromScratch - playing around with JQuery functionality.

--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/wwwroot/js/index.js
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/wwwroot/js/index.js
@@ -1,1 +1,11 @@
-﻿alert("Hello App!!");
+﻿$(document).ready(function () { 
+	alert("Hello App!!");
+
+	let $imgMain = $("#img-main");
+
+	$("#test-button").on("click", function () {
+		console.log(`You clicked on: ${$(this).text()}`);
+		$imgMain.toggle();
+		});
+
+});
