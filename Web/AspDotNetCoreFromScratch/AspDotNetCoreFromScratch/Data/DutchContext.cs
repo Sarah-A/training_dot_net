b19b7a11dca5c18a8bfb5d9dcb8de0bc107c0614
SarahA <ashri.sarah@gmail.com>
2019-04-05 11:18:10 +1000

    AspDotNetCoreFromScratch - Seeded Orders using Overriding DbContext.OnModelCreating method for seeding small, static tables.

--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Data/DutchContext.cs
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Data/DutchContext.cs
@@ -16,5 +16,17 @@ namespace AspDotNetCoreFromScratch.Data
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>().HasData(new Order {
+                Id = 1,
+                OrderDate = DateTime.Now,
+                OrderNumber = "12345"                
+            });
+        }
     }
 }
---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Data/Migrations/20190405011422_SeedOrders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspDotNetCoreFromScratch.Migrations
+{
+    public partial class SeedOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "OrderDate", "OrderNumber" },
+                values: new object[] { 1, new DateTime(2019, 4, 5, 11, 14, 22, 40, DateTimeKind.Local).AddTicks(9240), "12345" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
