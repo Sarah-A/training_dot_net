f263effb2175aee5e788d8728f86e1c3bee14d15
SarahA <ashri.sarah@gmail.com>
2019-04-09 07:34:15 +1000

    AspDotNetCoreFromScratch - New WebAPI Controller - ProductsController

---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using AspDotNetCoreFromScratch.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreFromScratch.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _repository.GetProducts();
+            if (products != null)
+            {
+                return Ok(products);
+            }
+            else
+            {
+                return BadRequest("Failed to find products");
+            }
+        }
+
+    }
+}
