b20d6b297cee95977702825f12a8c562f2aafdbf
SarahA <ashri.sarah@gmail.com>
2019-04-16 09:06:34 +1000

    AspDotNetCoreFromScratch - added login/logout functionality

--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Controllers/AccountController.cs
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using AspDotNetCoreFromScratch.ViewModels;
+﻿using AspDotNetCoreFromScratch.Data;
+using AspDotNetCoreFromScratch.Data.Entities;
+using AspDotNetCoreFromScratch.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,25 +13,62 @@ namespace AspDotNetCoreFromScratch.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<StoreUser> _signInManager;
         private readonly ILogger<AccountController> _logger;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(SignInManager<StoreUser> signInManager,
+                                ILogger<AccountController> logger)
         {
+            _signInManager = signInManager;
             _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Login()
         {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel loginViewModel)
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName,
+                    loginViewModel.Password,
+                    loginViewModel.RememberMe,
+                    false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+
+            // This will add this error to the model, to be displayed in the <asp-validation-summary="ModelOnly">
+            // in the login view.
+            ModelState.AddModelError("", "Wrong user name or password!");
 
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
         }
     }
 }
