0638b432bb6a8942bc2baf543053e3f3d75ad7f4
SarahA <ashri.sarah@gmail.com>
2019-04-16 06:48:06 +1000

    AspDotNetCoreFromScratch - added new login page. No functionality yet!

---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using AspDotNetCoreFromScratch.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreFromScratch.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            return View();
+        }
+    }
+}
