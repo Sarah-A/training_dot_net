66f0ef42c97841b2923eb49476e1331ed96cfbc5
SarahA <ashri.sarah@gmail.com>
2019-04-17 09:18:54 +1000

    AspDotNetCoreFromScratch - added typescript support and an example code.

---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/wwwroot/ts/storecustomer.ts
@@ -0,0 +1,25 @@
+﻿class StoreCustomer {
+
+	public visits: number = 0;
+	private ourName: string;
+
+	constructor(private firstName: string, private lastName: string) {
+	}
+
+
+	public showName()  {
+		alert(this.firstName + " " + this.lastName);	
+	}
+
+	set name(val: string) {
+		this.ourName = val;
+	}
+
+	get name() {
+		return this.ourName;
+	}
+
+
+
+
+}
