4c5f9f7db33d72722adaf971502b982d182ab91e
SarahA <ashri.sarah@gmail.com>
2019-04-24 08:49:10 +1000

    AspDotNetCoreFromScratch - Added a new Angular component and used some of Angular's html display syntax

---
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shop/productList.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from "@angular/core"
+
+@Component({
+	selector: "product-list",
+	templateUrl: "./productList.component.html",
+	styleUrls: []
+})
+export class ProductList {
+	public products = [{
+		title: "1st Product",
+		price: 23.56
+	},{
+		title: "2nd Product",
+		price: 39.21
+	}, {
+		title: "3rd Product",
+		price: 5.99
+	}]
+}
