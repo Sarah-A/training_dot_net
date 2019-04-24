420e323cf2d8095bbd40bb4ee7ecc17909b81bf2
SarahA <ashri.sarah@gmail.com>
2019-04-24 10:59:35 +1000

    AspDotNetCoreFromScratch - refactoring only - added TS types for type-safety

--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shared/dataService.ts
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shared/dataService.ts
@@ -1,15 +1,17 @@
 ﻿import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Product } from './product';
 
 // This class will contain all the data that is shared between the different components:
 @Injectable()
 export class DataService {
 	constructor(private http: HttpClient) { }
 
-	public products = [];
+	public products : Product[] = [];
 
-	loadProducts() {
+	loadProducts(): Observable<boolean> {
 		return this.http.get("/api/products")			// returns an observable that clients can subscribe to to be notified on changes
 			.pipe(
 			map((data: any[]) => {
