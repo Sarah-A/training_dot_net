eb0cd055c41e264c853b639380501b18c11f1aad
SarahA <ashri.sarah@gmail.com>
2019-04-24 10:31:39 +1000

    AspDotNetCoreFromScratch - connected the DataService to retrieve the products list form the server via HttpClient

--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shared/dataService.ts
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shared/dataService.ts
@@ -1,13 +1,21 @@
-﻿// This class will contain all the data that is shared between the different components:
+﻿import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+// This class will contain all the data that is shared between the different components:
+@Injectable()
 export class DataService {
-	public products = [{
-		title: "1st Product",
-		price: 23.56
-	}, {
-		title: "2nd Product",
-		price: 39.21
-	}, {
-		title: "3rd Product",
-		price: 5.99
-	}];
+	constructor(private http: HttpClient) { }
+
+	public products = [];
+
+	loadProducts() {
+		return this.http.get("/api/products")			// returns an observable that clients can subscribe to to be notified on changes
+			.pipe(
+			map((data: any[]) => {
+				this.products = data;
+				return true;
+			})
+			);
+	}
 }
--- Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shop/productList.component.ts
+++ Web/AspDotNetCoreFromScratch/AspDotNetCoreFromScratch/ClientApp/app/shop/productList.component.ts
@@ -1,16 +1,24 @@
-﻿import { Component } from "@angular/core"
+﻿import { Component, OnInit } from "@angular/core"
 import { DataService } from '../shared/dataService';
+
 
 @Component({
 	selector: "product-list",
 	templateUrl: "./productList.component.html",
 	styleUrls: []
 })
-export class ProductList {
+export class ProductList implements OnInit {
 
-	constructor(private data: DataService) {
-		this.products = data.products;
-	}
+	constructor(private data: DataService) {}
 
 	public products = [];
+
+	ngOnInit(): void {
+		this.data.loadProducts()
+			.subscribe(success => {
+				if (success) {
+					this.products = this.data.products;
+				}
+			});
+	}
 }
