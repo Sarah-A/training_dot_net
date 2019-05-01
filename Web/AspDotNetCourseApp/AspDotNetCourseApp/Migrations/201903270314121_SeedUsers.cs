9e6cbdec9a585850c8d22af36a463b04d4cab7df
SarahA <ashri.sarah@gmail.com>
2019-05-02 08:37:20 +1000

    AspDotNetCourseApp - Changed the default users to be: Admin: admin@example.com and Regular user: user@example.com. Both with password: P@ssw0rd!

--- Web/AspDotNetCourseApp/AspDotNetCourseApp/Migrations/201903270314121_SeedUsers.cs
+++ Web/AspDotNetCourseApp/AspDotNetCourseApp/Migrations/201903270314121_SeedUsers.cs
@@ -11,10 +11,10 @@ namespace AspDotNetCourseApp.Migrations
                 INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'4d7472d7-d025-4746-92a2-eaedcf73efc6', N'Admin')
                 INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'99dc0361-691e-4f3e-8ebb-9b1be5b23dc9', N'CanManageMovies')
 
-                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'116b8e29-2e96-4719-9ce4-fb065af439ad', N'guest.sarah@gmail.com', 0, N'AADJ23fRUwuYutvUKYzSL7lq55XaKhvUyseKoHYzxCoaQ6aCHEn7E8WrmjV9rIdEWg==', N'9d15b0a0-905c-41a8-9177-1560301d41ef', NULL, 0, 0, NULL, 1, 0, N'guest.sarah@gmail.com')
-                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'fe146c5d-b2fb-4f3b-95cc-04ed856e5932', N'admin.sarah@gmail.com', 0, N'AJfTjgt3+aiCyX6oMVT1ESZv/+hiy5MSKDfzSYt9E29mtTTnou3JOcx6z4XHgU6QlQ==', N'8abd4768-a14f-45df-a77c-7265479159d1', NULL, 0, 0, NULL, 1, 0, N'admin.sarah@gmail.com')
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'b7887e02-a6b5-4157-8fcc-d42c279039f0', N'admin@example.com', 0, N'ACI4nuUvfUPA3AgnFsbeLbsaIWbmYicXJQP0tH4tc/DaqTmZLG//3BEdNn4oODOhKg==', N'0e7d146b-5f4c-419e-a3da-978c657292c6', NULL, 0, 0, NULL, 1, 0, N'admin@example.com')
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'fc9800c3-4fa4-4c5f-9e94-239d933ea1f5', N'user@example.com', 0, N'AHZaPymEgDEYsMmBKIc6fzjKia9Z8YREZHje5n3EUUt9fq/4963gBI5tQM+1+ES30w==', N'71837623-a71a-4bca-b2d8-d9c91410d6cc', NULL, 0, 0, NULL, 1, 0, N'user@example.com')
 
-                INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'fe146c5d-b2fb-4f3b-95cc-04ed856e5932', N'4d7472d7-d025-4746-92a2-eaedcf73efc6')
+                INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'b7887e02-a6b5-4157-8fcc-d42c279039f0', N'4d7472d7-d025-4746-92a2-eaedcf73efc6')
 
             ");
         }
