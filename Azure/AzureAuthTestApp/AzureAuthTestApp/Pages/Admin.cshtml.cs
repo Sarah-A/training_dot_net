98cedcc14e649e6aa4c34a07464abd0afdf96657
Sarah Ashri <sarah.ashri@octopus.com>
2023-10-26 09:44:17 +1000

    AzureAuthTestApp: Add User Roles defined in Azure and wire up the authentication and authorization to get RBAC control into app

--- Azure/AzureAuthTestApp/AzureAuthTestApp/Pages/Admin.cshtml.cs
+++ Azure/AzureAuthTestApp/AzureAuthTestApp/Pages/Admin.cshtml.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace aspnetcoreapp.Pages;
 
+[Authorize(Roles = "AppRoles.Admin")]
 public class AdminModel : PageModel
 {
     private readonly ILogger<AdminModel> _logger;
--- Azure/AzureAuthTestApp/AzureAuthTestApp/Program.cs
+++ Azure/AzureAuthTestApp/AzureAuthTestApp/Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var configuration = builder.Configuration;
+
+// Initialise Authentication services for the app, using OpenIdConnect as the authentication scheme
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    // Use Azure AD as the identity provider, using the parameters defined in appsettings.json to communicate
+    // with the Azure AD instance:
+    .AddMicrosoftIdentityWebApp(configuration.GetSection("AzureAd"));   
+
+// // Enable support for web API and Razor pages in my app: 
+// builder.Services.AddControllersWithViews()
+//     // refresh pages after changing the razor pages immediately, without needing a restart
+//     .AddRazorRuntimeCompilation();
+    
+// Enable and user Razor pages:
+builder.Services.AddRazorPages()
+    // Add authentication policy and require users to be authenticated to use the app (for all pages)
+    // If an action/page is accessible without authentication, it will be decorated with [AllowAnonymous]
+    .AddMvcOptions(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    })
+    // Add default UI pages for sign in, sign out etc. This also add the nice error message if I try to access 
+    // a page I'm not authorised for (e.g. "Access Denied" instead of some generic http not found error)
+    .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
@@ -18,6 +49,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
