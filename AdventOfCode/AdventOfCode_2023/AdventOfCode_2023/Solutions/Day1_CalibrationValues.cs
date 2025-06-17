3ab65486b43cca517ae4e83d4a65ec9efdb86e93
Sarah Ashri <sarah.ashri@octopus.com>
2025-06-17 10:46:11 +1000

    AdventOfCode_2023

--- AdventOfCode/AdventOfCode_2023/AdventOfCode_2023/Solutions/Day1_CalibrationValues.cs
+++ AdventOfCode/AdventOfCode_2023/AdventOfCode_2023/Solutions/Day1_CalibrationValues.cs
@@ -1,7 +1,24 @@
+using System.Text;
+
 namespace AdventOfCode2023;
 
 public class Day1_CalibrationValues
 {
+    readonly Dictionary<string, string> digitReplacements =
+        new Dictionary<string, string>()
+        {
+            { "zero", "0" },
+            { "one", "1" },
+            { "two", "2" },
+            { "three", "3" },
+            { "four", "4" },
+            { "five", "5" },
+            { "six", "6" },
+            { "seven", "7" },
+            { "eight", "8" },
+            { "nine", "9" }
+        };
+    
     public int Calculate_Q1(string filePath)
     {
         var totalValue = 0;
@@ -16,10 +33,57 @@ public class Day1_CalibrationValues
         return totalValue;
     }
 
+    public int Calculate_Q2(string filePath)
+    {
+        var totalValue = 0;
+        using (var reader = new StreamReader(filePath))
+        {
+            while (reader.ReadLine() is { } line)
+            {
+                var reformattedLine = TranslateLexicalValuesToDigits(line);
+                totalValue += GetLineValue(reformattedLine);
+            }
+        }
+
+        return totalValue;
+    }
+
+    private string TranslateLexicalValuesToDigits(string inputString)
+    {
+        var result = new StringBuilder();
+        inputString = inputString.ToLower();
+
+        for (var i= 0; i< inputString.Length ; i++)
+        {
+            if (Char.IsDigit(inputString[i]))
+            {
+                result.Append(inputString[i]);
+            }
+            else
+            {
+                foreach (var keyValuePair in digitReplacements)
+                {
+                    if (inputString.StartsWith(keyValuePair.Key))
+                    {
+                        result.Append(keyValuePair.Value);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // var result = digitReplacements.Aggregate(inputString,
+        //     (current, replacement) => current.Replace(replacement.Key, replacement.Value));
+
+        return result.ToString();
+
+    }
+
     private int GetLineValue(string line)
     {
-        var firstDigit = line.First(c => Char.IsDigit(c));
-        var lastDigit = line.Last(c => Char.IsDigit(c));
+        var firstDigit = line.FirstOrDefault(c => Char.IsDigit(c));
+        var lastDigit = line.LastOrDefault(c => Char.IsDigit(c));
+        if(firstDigit == 0 && lastDigit == 0) return 0;
         return int.Parse($"{firstDigit}{lastDigit}");
     }
 }
--- AdventOfCode/AdventOfCode_2023/UnitTests/Q1_Fixture.cs
+++ AdventOfCode/AdventOfCode_2023/UnitTests/Q1_Fixture.cs
@@ -5,10 +5,10 @@ using NUnit.Framework;
 namespace UnitTests;
 
 [TestFixture]
-public class Q1Fixture
+public class Day1Fixture
 {
     [Test]
-    public void CalculateTestValues()
+    public void CalculateTestValues_Q1()
     {
         var q1 = new Day1_CalibrationValues();
         var result = q1.Calculate_Q1(
@@ -16,12 +16,32 @@ public class Q1Fixture
         result.Should().Be(319);
 
     }
-
+    
     [Test]
-    public void CalculateFinalValues()
+    public void CalculateFinalValues_Q1()
     {
         var q1 = new Day1_CalibrationValues();
         var result = q1.Calculate_Q1(
+            "C:\\MyFiles\\Training\\my-github-dot-net-training\\AdventOfCode\\AdventOfCode_2023\\AdventOfCode_2023\\Data\\Q1.txt");
+        Console.WriteLine($"Result for submission: {result}");
+
+    }
+    
+    [Test]
+    public void CalculateTestValues_Q2()
+    {
+        var q1 = new Day1_CalibrationValues();
+        var result = q1.Calculate_Q2(
+            "C:\\MyFiles\\Training\\my-github-dot-net-training\\AdventOfCode\\AdventOfCode_2023\\UnitTests\\TestData\\test-data-Q2.txt");
+        result.Should().Be(281);
+
+    }
+    
+    [Test]
+    public void CalculateFinalValues_Q2()
+    {
+        var q1 = new Day1_CalibrationValues();
+        var result = q1.Calculate_Q2(
             "C:\\MyFiles\\Training\\my-github-dot-net-training\\AdventOfCode\\AdventOfCode_2023\\AdventOfCode_2023\\Data\\Q1.txt");
         Console.WriteLine($"Result for submission: {result}");
 
